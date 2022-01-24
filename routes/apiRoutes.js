36464db5ef5f180ba3ef90a8cc7ae3ba9b5f0db3
lucasmo066 <lucasmo066@gmail.com>
2022-01-23 20:43:14 -0500

    require fs

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,7 @@ const router = require("express").Router();
 const { notes } = require("../db/db.json");
 const { createNewNote, deleteNote } = require("../lib/notes");
 
+// GET req for notes.html
 router.get("/notes", (req, res) => {
     if (notes) {
         res.send(notes);
@@ -10,14 +11,13 @@ router.get("/notes", (req, res) => {
         res.json("close");
     }
 }); 
-
+// GET * returning index.html
 router.post("/notes", (req, res) => {
     req.body.id = notes.length.toString();
 
     const note = createNewNote(req.body, notes);
 
     res.json(note);
-    
 });
 
 router.delete("/notes/:id", (req, res) => {
